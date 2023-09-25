d0d11e0ef2f4964b3a815248823e521aa8a0528f
vitor.gama <vitorggam@hotmail.com>
2023-09-25 11:43:44 -0300

    banco de dados db4free

--- src/banco.js
+++ src/banco.js
@@ -1,12 +1,12 @@
 import mysql2 from 'mysql2';
 
 //Armazendo os dados da conexão
-const conexao = mysql2.createConnection({
+/*const conexao = mysql2.createConnection({
     host : 'localhost',
     user : 'root',
     password : '',
     database : 'escola'
-});
+});*/
 
 //Efetivando a conexão
 //conexao.connect();
@@ -14,8 +14,16 @@ conexao.connect( erro => {
     if( erro ){
         console.error(`erro ao conectar: ${erro.message}`)
     }else{
-        console.log(`Banco de dados conectado com sucesso!`)
+        console.log(`Banco de dados conectado em: ${conexao.config.host}`);
     }
+});
+
+//Banco de dados db4free
+const conexao = mysql2.createConnection({
+    host : 'db4free.net',
+    user : 'vitorsenac',
+    password : 'Senac123',
+    database : 'apiescolavitor'
 });
 
 export default conexao;
