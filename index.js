5778a92809f58b998fc410cbad8a6afe12b97af1
vitor.gama <vitorggam@hotmail.com>
2023-09-27 11:53:33 -0300

    permitindo acesso aos arquivos da API
    CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ler, inserir, lerUm, atualizar, excluir } from './src/aluno.js';
+import cors from 'cors';
 const app = express();
 const porta = process.env.PORT || 3306; //permtiindo que o servidor aponte a mehor porta // então use a porta 3306
 
@@ -8,6 +9,9 @@ app.use(express.json());
 
 //adicionando suporte a dados
 app.use(express.urlencoded({ extended : true}));
+
+//permitindo acesso aos arquivos da API
+app.use(cors());
 
 
 // Defina uma rota HTTP GET para a raiz ('/') do seu aplicativo.
